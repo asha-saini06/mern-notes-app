ef67f9decdc99fabd17b5fe86d1617ba9a742d71
Asha Saini <sainiasha.ash@gmail.com>
2026-01-10 22:51:50 +0530

    Server endpoints and nodemon integration

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,29 @@ import express from 'express';
 
 const app = express();
 
+// Endpoint : An endpoint is a combination of a URL and an HTTP method that allows clients to interact with a server.
+// Here, we are defining a GET endpoint at the path '/api/notes'
 app.get('/api/notes', (req, res) => {
     // send the notes as a response
-  res.send('Notes is working');
+  res.status(200).send('Notes is working!!');
 });
 
 app.post('/api/notes', (req, res) => {
-    // add a new note
-  res.send('Add a new note');
+    // handle POST request to create a new note
+  res.status(201).json('Note created successfully!');
 });
 
+app.put('/api/notes/:id', (req, res) => {
+    // handle PUT request to update an existing note
+  res.status(200).json('Note updated successfully!');
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    // handle DELETE request to delete a note
+  res.status(200).json('Note deleted successfully!');
+});
+
+// Start the server and listen on port 5001
 app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+  console.log('Server is running on PORT: 5001');
 });
